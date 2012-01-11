944a0ac85dbd84ef1bf937a5a7c2096d7bb4dbf3
Denis V. Bakaev <Kiragods@gmail.com>
2012-01-11 13:27:20 +0200

    one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbvrez::Application.config.secret_token = '8ede313507227ea42c47e5e28f9d33633ec404b62978a385e2a0e76ac24a4c5541b93630ae29f5eee07a331d9da2433e1f73d751a4cadc2340666f9940f0b7fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dbvrez::Application.config.session_store :cookie_store, key: '_dbvrez_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dbvrez::Application.config.session_store :active_record_store
